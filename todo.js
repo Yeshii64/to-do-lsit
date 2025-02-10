f06e82b884de055ebb0f1834302bd08155042cb4
Yeshua! <yeshuaafflick5@gmail.com>
2025-02-10 17:31:00 -0500

    finished css, on to the javaScript portion!

--- todo.js
+++ todo.js
@@ -1,8 +1,20 @@
+//go to the todo list page
 function menuRedirect(){
     window.location.href = "todo.html";
 }
 
-
+//returning to the menu page
 function returnMenuPage(){
     window.location.href="menu.html";
+}
+
+
+//creating the task function
+function addTask(){
+    const input = document.getElementById("task");
+    const inputValue = input.value;
+
+    if(inputValue === ""){
+        alert("Please enter a task");
+    }
 }
