b04e0add061d3dc6caf4b7f2858ed4217640545c
Yeshua! <yeshuaafflick5@gmail.com>
2025-02-13 11:20:04 -0500

    OKAY IT WORKED (KINDVE), but it shows up something else?

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+let localStoragedata = localStorage.getItem("task_list");
 //go to the todo list page
 function menuRedirect(){
     window.location.href = "todo.html";
@@ -8,15 +9,35 @@ function returnMenuPage(){
     window.location.href="menu.html";
 }
 
-//creating a check symbol when the user clicks on the task
-var myList = document.getElementsByTagName("ul");
-var i;
-for(i = 0; i < myList.length; i++){
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
+
+//creating the task function
+function addTask(){
+    const input = document.getElementById("task");
+    const inputValue = input.value;
+    var ul = document.getElementById("task_list"); //getting the ul element
+    var li = document.createElement("li"); //creating the li element
+    var textNode = document.createTextNode(inputValue);
+    li.appendChild(textNode) //adding what the user entered to the li element
+
+    if(inputValue === ""){   //input validation
+        alert("Please enter a task");
+    }else{
+        ul.appendChild(li);
+        }
+
+    input.value=""; //clearing the input field
+    
+
+    localStorage.setItem("task_list", JSON.stringify(task_list)); //storing the task in local storage
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7"); //creates a text node with the x symbol
     span.className = "close";
     span.appendChild(txt);
-    myList[i].appendChild(span);
+    li.appendChild(span);
+
+    addCloseFunction(); //calling the close function  
+    updateLocalStorage(); //updating the local storage
 }
 
 //making the close button actually close the task
@@ -26,35 +47,22 @@ function addCloseFunction(){
         close[i].onclick = function(){
             var div = this.parentElement;
             div.style.display = "none";
+            updateLocalStorage();
         }
     }
 }
 
-
-//creating the task function
-function addTask(){
-    const input = document.getElementById("task");
-    const inputValue = input.value;
-    var ul = document.getElementById("task_list"); //getting the ul element
-    var li = document.createElement("li"); //creating the li element
-    var Y = document.createTextNode(inputValue);
-    li.appendChild(Y) //adding what the user entered to the li element
-
-    if(inputValue === ""){   //input validation
-        alert("Please enter a task");
-    }else{
-        document.getElementById("task_list").appendChild(li);
-        }
-
-    document.getElementById("task").value = "";
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7"); //creates a text node with the x symbol
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    addCloseFunction(); //calling the close function
+function updateLocalStorage(){
+  var ul= document.getElementById("task_list");
+  localStorage.setItem("task_list", ul.innerHTML);
 }
 
-addCloseFunction();
+window.onload = function(){
+    if(localStoragedata != null){
+        var ul = document.getElementById("task_list");
+        ul.innerHTML = localStoragedata;
+        addCloseFunction();
+    }
+}
+
 
