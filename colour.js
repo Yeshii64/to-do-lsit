2ede15470825e2dc9b656dd557ca53c87d8c3b89
Yeshua! <yeshuaafflick5@gmail.com>
2025-03-28 11:53:55 -0400

    i finished, finished the colour changing thing with multiple colours!

--- colour.js
+++ colour.js
@@ -1,9 +1,11 @@
 //this file is to change the colour of the background (with the button in the menu.html file)
 //maybe in the future i can implement an api?
+
+
+let colorPlace = 0;
 function changeColor(){
-    let validColours = ["#33ff39", "#33fffc", "#ff5233"];
-    let currentColour = 1;
-    document.body.style.backgroundColor = validColours;
-    currentColour++;
-    console.log("changed colour");   
+    const validColours = ['#1ecbe1','#e0341f','#befc03','#4706f9'];
+    document.body.style.backgroundColor = validColours[colorPlace];
+    colorPlace = (colorPlace + 1) % validColours.length;
+
 }
